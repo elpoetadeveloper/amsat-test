d879d3987345e8a745859228adaaafb62adf0355
elpoetadeveloper <46660619+elpoetadeveloper@users.noreply.github.com>
2025-06-11 07:22:51 -0300

    Update run_updater.sh

--- scripts/run_updater.sh
+++ scripts/run_updater.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-mkdir -p  /var/amsat/data/passes/html
-mkdir -p  /var/amsat/data/passes/txt
+IT_DIR="/var/amsat/data/github/it"
+PASSES_HTML_DIR="/var/amsat/data/github/passes/html"
+PASSES_TXT_DIR="/var/amsat/data/github/passes/txt"
+JAVA_KEPS_UPDATER_DIR="/var/amsat/data/github/keps_updater"
+SCRIPTS_DIR="/var/amsat/data/github/scripts"
+
+mkdir -p "${PASSES_HTML_DIR}" 
+mkdir -p "${PASSES_TXT_DIR}" 
 
 # List of script base names (without .bat)
 #SCRIPTS=("subePasoPart1" "subePasoPart2" "subePasoPart3" "subePasoPart4" "subePasoPart5" "subePasoPart6" "subePasoPart7" "subePasoPart8" "subePasoPart9" "subePasoPart10")
 SCRIPTS=("subePasoPart1")
 
 shopt -s nocaseglob
-htm_files=(/var/amsat/data/it/pasos*.htm)
-txt_files=(/var/amsat/data/it/pasos*.txt)
+htm_files=("${IT_DIR}/pasos*.htm")
+txt_files=("${IT_DIR}/pasos*.txt")
 shopt -u nocaseglob
 
 copy_and_lowercase() {
@@ -34,29 +40,28 @@ copy_and_lowercase() {
 
 # Process HTML files
 if (( ${#htm_files[@]} )); then
-    copy_and_lowercase "/var/amsat/data/passes/html" "${htm_files[@]}"
+    copy_and_lowercase "$PASSES_HTML_DIR" "${htm_files[@]}"
 else
     echo "[WARNING] $(date '+%Y-%m-%d %H:%M:%S') - No matching .htm files found."
 fi
 
 # Process TXT files
 if (( ${#txt_files[@]} )); then
-    copy_and_lowercase "/var/amsat/data/passes/txt" "${txt_files[@]}"
+    copy_and_lowercase "${PASSES_TXT_DIR}" "${txt_files[@]}"
 else
     echo "[WARNING] $(date '+%Y-%m-%d %H:%M:%S') - No matching .txt files found."
 fi
 
 #
 #Execute java update keps
-rm -R /var/amsat/data/keps_updater/target
-mkdir -p /var/amsat/data/keps_updater/target
-javac -d /var/amsat/data/keps_updater/target /var/amsat/data/keps_updater/src/amsat/*.java
-java -cp /var/amsat/data/keps_updater/target amsat.KepsUpdateRunner
+rm -R "${JAVA_KEPS_UPDATER_DIR}/target"
+mkdir -p "${JAVA_KEPS_UPDATER_DIR}/target"
+javac -d "${JAVA_KEPS_UPDATER_DIR}/target" "${JAVA_KEPS_UPDATER_DIR}/src/amsat/*.java"
+java -cp "${JAVA_KEPS_UPDATER_DIR}/target" amsat.KepsUpdateRunner
 
 for script in "${SCRIPTS[@]}"; do
   echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') - Running $script.bat batch script..."
-  /bin/bash  /var/amsat/data/scripts/run_dosbox.sh "$script"
+  /bin/bash  "${SCRIPTS_DIR}/run_dosbox.sh" "$script"
   sleep 2
 done
-
 
