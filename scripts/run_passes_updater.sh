8cb75bb2fec459286d1409858dbddac31fda9aad
elpoetadeveloper <46660619+elpoetadeveloper@users.noreply.github.com>
2025-06-27 13:42:27 -0300

    Update run_passes_updater.sh

--- scripts/run_passes_updater.sh
+++ scripts/run_passes_updater.sh
@@ -3,8 +3,8 @@
 SCRIPTS_DIR="/var/amsat/data/github/scripts"
 
 # List of script base names (without .bat)
-#SCRIPTS=("subePasoPart1" "subePasoPart2" "subePasoPart3" "subePasoPart4" "subePasoPart5" "subePasoPart6" "subePasoPart7" "subePasoPart8" "subePasoPart9" "subePasoPart10")
-SCRIPTS=("subePasoPart1" "subePasoPart2" "subePasoPart3")
+SCRIPTS=("subePasoPart1" "subePasoPart2" "subePasoPart3" "subePasoPart4" "subePasoPart5" "subePasoPart6" "subePasoPart7" "subePasoPart8" "subePasoPart9" "subePasoPart10")
+#SCRIPTS=("subePasoPart1" "subePasoPart2" "subePasoPart3")
 
 for script in "${SCRIPTS[@]}"; do
   echo "[INFO] $(date '+%Y-%m-%d %H:%M:%S') - Running $script.bat batch script..."
